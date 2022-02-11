c240f1fa94f25d8088285970d843142a7fd4cd69
iqbal Rafiud Drajat <97111812+iqbalRD@users.noreply.github.com>
2022-02-11 21:25:59 +0700

    Update login-script.js
    mengganti komentar sesuai dengan format JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -5,17 +5,53 @@
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
 
+/** @module Login-Script */
+/**
+* Membuat Variable loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+
+/**
+* Membuat Variable inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/**
+* Membuat Variable inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+
+/**
+* Membuat Variable expectedEmail untuk menyimpan informasi email sementara.
+* @constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+
+/**
+* Membuat Variable expectedPassword untuk menyimpan informasi password sementara.
+* @constant {string}
+*/
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
+  /**
+  * Membuat Variable email untuk menyimpan nilai email yang didapatkan saat button di tekan.
+  * @constant {string}
+  */
   const email = inputEmailElement.value;
+  
+  /**
+  * Membuat Variable password untuk menyimpan nilai password yang didapatkan saat button di tekan.
+  * @constant {string}
+  */
   const password = inputPasswordElement.value;
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
